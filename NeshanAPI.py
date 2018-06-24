87714357fbd15d90612b2f163aeb2c140cd59f05
BenYamin Salimi <benyamin.salimi@gmail.com>
2018-06-24 23:07:45 +0430

    tested
    Signed-off-by: BenYamin Salimi <benyamin.salimi@gmail.com>

--- NeshanAPI.py
+++ NeshanAPI.py
@@ -1,11 +1,14 @@
 import requests
 
+__author__='Benyamin.Salimi@gmail.com'
+__repository__='https://github.com/benyaminsalimi/Neshan-Python'
 
 class NeshanAPI(object):
     def __init__(self,key):
         self.header = {}
         self.header['Api-Key'] = key
         self.header['user-agent'] = 'Neshan API Python lib'
+        self.header['Content-Type'] = 'application/x-www-form-urlencoded'
         self.api_url = 'https://api.neshan.org/v1/'
 
     def Test_Api_Key(self):
@@ -33,34 +36,34 @@ class NeshanAPI(object):
             print('Something wrong')
             return False
 
-    def Location_Based(term, latitude, longitude,self):
-        url = self.api_url +'search'
+    def req(self,url,param=None):
+        url = self.api_url + url
+        req = requests.get(url=url, headers=self.header, params=param)
+        result = req.json()
+        status_code = req.status_code
+        return result, status_code
+
+    def Location_Based(self, term, latitude, longitude):
+        url = 'search'
         param = {}
         param['term'] = term
         param['lat'] = latitude
         param['lng'] = longitude
-        req = requests.get(url=url, headers=self.header , params=param)
-        return req.json()
+        return self.req(url=url,param=param)
 
     def Reverse_Geocoding(self,latitude, longitude,):
-        url = self.api_url +'reverse'
+        url = 'reverse'
         param = {}
         param['lat'] = latitude
         param['lng'] = longitude
-        print(self.header)
-        req = requests.get(url=url, headers=self.header , params=param)
-        return req.json()
+        return self.req(url=url, param=param)
 
     def Direction(self,origin_latitude,origin_longitude,destination_latitude,destination_longitude,alternative):
-        url = self.api_url + 'routing'
+        url = 'routing'
         param = {}
-
         if alternative is True:
             param['alternative'] = 'true'
-
         # latitude,longtitude
         param['origin'] = origin_latitude+','+origin_longitude
         param['destination'] = destination_latitude+','+destination_longitude
-
-        req = requests.get(url=url, headers=self.header, params=param)
-        return req.json()
+        return self.req(url=url, param=param)
--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 from NeshanAPI import *
 
-api = NeshanAPI('')
-result=api.Reverse_Geocoding('32.86677831625764','59.21867264174193')
+api = NeshanAPI('your API KEY')
+test = api.Location_Based('رضا','59.6157432','36.2880443')
+result= test[0]
+status_code= test[1]
 print(result)
+print(status_code)
